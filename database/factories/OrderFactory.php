ef3a9840849c30208a526eec25887cfee62b3f5d
Stefano Patti <mastepa@hotmail.com>
2021-06-15 19:03:14 +0200

    Factory - Type, Order, Product

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Order;
+use Faker\Generator as Faker;
+
+$factory->define(Order::class, function (Faker $faker) {
+    return [
+        'firstname' => $faker -> firstName,
+        'lastname' => $faker -> lastName,
+        'email' => $faker-> unique() -> safeEmail,
+        'telephone' => '+39' . $faker -> numberBetween(300000000, 399999999) . $faker -> randomNumber(1),
+        'address' => $faker -> address(),
+        'delivery_date' => $faker -> dateTimeBetween('-1 years', 'now', null),
+        'total_price' => '€' . $faker -> numberBetween(1, 200),
+    ];
+});
