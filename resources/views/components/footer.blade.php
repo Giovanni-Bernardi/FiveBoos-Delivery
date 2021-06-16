5ff0cc5b9c6125baca149ae000d7f58b6b5cd34b
Davide Mannarelli <davidemannarelli24@gmail.com>
2021-06-16 16:54:19 +0200

    Footer: general fixes

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
-    <div class="deivh-container">
+    <div class="ms-container">
         <div class="top">
 
             <div class="logo">
@@ -7,8 +7,8 @@
                     Delive<span>Boo</span> 
                 </h3>
                 <div class="stores">
-                    <img src="{{ asset('/storage/assets/applestore.png') }}" alt="logo apple store">
-                    <img src="{{ asset('/storage/assets/playstore.png') }}" alt="logo play store">
+                   <a href=""> <img src="{{ asset('/storage/assets/applestore.png') }}" alt="logo apple store"> </a> 
+                   <a href=""> <img src="{{ asset('/storage/assets/playstore.png') }}" alt="logo play store"> </a>
                 </div>
             </div>
     
@@ -19,7 +19,7 @@
                             <li> <a href="#"> Informazioni su DeliveBoo </a></li>
                             <li> <a href="#"> Leggi il nostro blog </a></li>
                             <li> <a href="#"> Crea un account</a></li>
-                            <li> <a href="#"> Aggiungi il tuo ristorande</a></li>
+                            <li> <a href="#"> Aggiungi il tuo ristorante</a></li>
                             <li> <a href="#"> Registrati per consegnare </a></li>
                         </ul>
                     </div>
@@ -37,27 +37,29 @@
             </div>
         </div>
 
-        
-        <div class="bottom">
-            <div class="icons">
-                <a href="#"> <i class="fab fa-facebook-f"></i> </a>
-                <a href=""> <i class="fab fa-twitter"></i></a>
-                <a href=""> <i class="fab fa-instagram"></i></a>
+        <div class="border-footer-divider">
+            
+        </div>
+    </div>
+    <div class="bottom">
+        <div class="icons">
+            <a href="#"> <i class="fab fa-facebook-f"></i> </a>
+            <a href=""> <i class="fab fa-twitter"></i></a>
+            <a href=""> <i class="fab fa-instagram"></i></a>
+        </div>
+
+
+        <div class="privacy">
+            <div class="top">
+                <a href="#"> Informativa sulla privacy </a>
+                <a href="#"> Termini e condizioni</a>
+                <a href="#"> Tariffe </a>
             </div>
-
-
-            <div class="privacy">
-                <div class="top">
-                    <a href="#"> Informativa sulla privacy </a>
-                    <a href="#"> Termini e condizioni</a>
-                    <a href="#"> Tariffe </a>
-                </div>
-                <div class="copyright">
-                    <span>
-                        <i class="far fa-copyright"></i>
-                        DeliveBoo Technologies, Team5 Inc.
-                    </span>
-                </div>
+            <div class="copyright">
+                <span>
+                    <i class="far fa-copyright"></i>
+                    DeliveBoo Technologies, Team5 Inc.
+                </span>
             </div>
         </div>
     </div>
