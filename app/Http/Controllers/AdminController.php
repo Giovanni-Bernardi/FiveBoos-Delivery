177d8da10c697be77fc1a334a82b700b40a7fe4d
Stefano Patti <mastepa@hotmail.com>
2021-06-16 12:28:17 +0200

    create Restaurant

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Restaurant;
 
 class AdminController extends Controller
 {
@@ -11,4 +14,55 @@ class AdminController extends Controller
         $this->middleware('auth');
     }
     
+    // CREA SINGOLO RISTORANTE
+    public function createRestaurant()
+    {
+        return view('pages.restaurant-create');
+    }
+    // ---------------------------------
+    public function storeRestaurant(Request $request)
+    {
+        // dd($request -> all());
+        $validate = $request -> validate([
+        'business_name' => 'required|string',
+        'piva' => 'required|string',
+        'address' => 'required|string',
+        'description' => 'required|string',
+        'telephone' => 'required|string',
+        ]);
+        // dd($validate);
+        $restaurant = Restaurant::make($validate);
+
+        $id = Auth::id();
+
+        $restaurant -> user() -> associate($id);
+
+        $restaurant -> save();
+
+        // passo l'img del form dell html in file
+        $img = $request -> file('img');
+
+        // mi estraggo l'extension del file
+        $imgExt = $img -> getClientOriginalExtension();
+
+        // modifico nome img
+        $imgNewName = time() . rand(0, 1000). '.' . $imgExt;
+
+        // creo il percorso
+        $folder = '/restaurant-img/';
+
+        // e le salvo lato server
+        $imgFile = $img -> storeAs($folder, $imgNewName, 'public');
+
+        $restaurant -> img = $imgNewName;
+
+        $restaurant -> save();
+
+
+        // dd($img, $imgNewName, $imgFile);
+
+
+
+        return redirect() -> route('indexViewLink');
+    }
 }
