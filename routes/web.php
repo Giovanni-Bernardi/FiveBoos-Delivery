c23f2130df9f55d9d5463c48e73ab90c154dcfa8
Vladyslav Okopnyy <harmfulcati@gmail.com>
2021-06-16 16:50:43 +0300

    Add edit product, Modification gitignore, change web.php

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ Route::get('/index','RestaurantController@indexView') -> name('indexViewLink');
 // Route dettaglio ristorante con lista prodotti
 Route::get('/restaurant/{id}','RestaurantController@restaurantDetailsView') -> name('restaurantDetailsViewLink');
 
+// Route dettaglio prodotto
+Route::get('/restaurant/product/{id}','RestaurantController@productDetailsView') -> name('productDetailsViewLink');
+
 // Route per creazione di Restaurant
 Route::get('restaurant-create', 'AdminController@createRestaurant') -> name('createRestaurant');
 Route::post('restaurant-store', 'AdminController@storeRestaurant') -> name('storeRestaurant');
@@ -31,7 +34,6 @@ Route::get('/edit/restaurant/{id}','AdminController@editRestaurantView') -> name
 // Route salvataggio/store edit ristorante
 // Route::post('/store/restaurant/{id}','AdminController@editRestaurantView') -> name('editRestaurantViewLink');
 
-// Route dettaglio prodotto
-Route::get('/restaurant/product/{id}','RestaurantController@productDetailsView') -> name('productDetailsViewLink');
 // Route edit prodotto
 Route::get('/edit/product/{id}','AdminController@editProductView') -> name('editProductViewLink');
+Route::post('/update/product/{id}', 'AdminController@updateProductView') -> name('updateProductViewLink');
