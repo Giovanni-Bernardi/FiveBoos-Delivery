2ade83346dc5ed877e7c33e94de8228afcf968ea
Vladyslav Okopnyy <harmfulcati@gmail.com>
2021-06-21 17:14:41 +0300

    Beta function new order

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Restaurant;
 use App\Product;
 use App\Type;
+use App\Order;
 
 class RestaurantController extends Controller
 {
@@ -29,12 +30,41 @@ class RestaurantController extends Controller
     // Pagina di prova del carello per tutti ristoranti
     public function restaurantPublic(){
         $restaurants = Restaurant::all();
-        return view('pages.restaurant-public', compact('restaurants'));
+        $products = Product::all();
+        $orders = Order::all();
+        return view('pages.restaurant-public', compact('restaurants', 'products', 'orders'));
     }
 
     // Pagina di prova del carello per gli piatti del ristorante
     public function restaurantDetailsPublic($id){
         $restaurant = Restaurant::findOrFail($id);
-        return view('pages.restaurant-product-public', compact('restaurant'));
+        $products = Product::all();
+        $orders = Order::all();
+        return view('pages.restaurant-product-public', compact('restaurant', 'products', 'orders'));
+    }
+
+    public function storeOrder(Request $request) {
+        // $products = Product::all();
+
+        $validate = $request -> validate([
+          'firstname' => 'required|string|min:3',
+          'lastname' => 'required|string|min:3',
+          'email' => 'required|string',
+          'telephone' => 'required|string',
+          'address' => 'required|string',
+          'delivery_date' => 'required|date',
+          'total_price' => 'required|integer'
+        ]);
+
+        $restaurant = Restaurant::findorFail($request -> get('restaurant_id'));
+
+        $order = Order::make($validate);
+        $product -> restaurant() -> associate($restaurant);
+        $order -> save();
+
+        $order -> products() -> attach($request -> get('products_id'));
+        $order -> save();
+
+        return redirect() -> route('indexViewLink');
     }
 }
