29a3ae47ed10c6049e4bd95376ee8f273e7fb58e
Stefano Patti <mastepa@hotmail.com>
2021-06-23 18:45:18 +0200

    bye bye page

--- resources/views/pages/byebye.blade.php
+++ resources/views/pages/byebye.blade.php
@@ -3,24 +3,36 @@
 
 @section('content')
 
+<head>
+    <meta http-equiv="refresh" content="5; url={{ route('indexViewLink') }}" />
+</head>
+
 <main>
     <h1 class="prova1">
-        pag esito finale
+        Grazie per aver scelto FIVEBOO'S il tuo ordine partirà a breve
     </h1>
 
-    <div class="movimento">
+    {{-- <div class="movimento">
 
             <div id = "pot">
                 <img src = "https://i.stack.imgur.com/qgNyF.png?s=328&g=1" width = "100px" height ="100px">
             </div>
 
+    </div> --}}
+
+    <div class="riders-img movimento">
+        <div id = "scooter">
+            <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+            <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_iemp22mw.json"  background="transparent"  speed="3" loop  autoplay></lottie-player>
+
+        </div>
+
     </div>
 
-    <div class="riders-img">
-
-        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-        <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_iemp22mw.json"  background="transparent"  speed="3" loop  autoplay></lottie-player>
+    <div class="home_route">
+        <a class="mx-2" href="{{ route('indexViewLink') }}"> <i class="fas fa-home"></i></a>
     </div>
+
 
 
     
