9d0c004cf74506dbe95de70d859ad580604091d8
Davide Mannarelli <davidemannarelli24@gmail.com>
2021-06-23 17:46:59 +0200

    Restaurant Details: bugfix

--- resources/views/pages/restaurant-details.blade.php
+++ resources/views/pages/restaurant-details.blade.php
@@ -130,6 +130,26 @@
                 Owner: {{$restaurant -> user -> email}}
             </u>
         </p>
+    
     </main>
+    {{-- <script>
+            function PopupModal(){
+        var modal = document.getElementById("myModal");
+        var btn = document.getElementById("myBtn");
+        var span = document.getElementsByClassName("close")[0];
+        btn.onclick = function() {
+        modal.style.display = "block";
+        }
+        span.onclick = function() {
+        modal.style.display = "none";
+        }
+        window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+        }
+    }
+    document.addEventListener('DOMContentLoaded', PopupModal);
+    </script> --}}
     @endsection
     
