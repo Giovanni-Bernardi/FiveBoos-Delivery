5a488be00b7d17f3d3e6d78cbe43b7ec33098944
Giovanni <giovannibernardi13@gmail.com>
2021-06-25 01:15:43 +0200

    my-profile page no @foreach

--- resources/views/pages/my-profile.blade.php
+++ resources/views/pages/my-profile.blade.php
@@ -6,7 +6,7 @@
   <main>
     <section class="side-padding" id="restaurant-container">
       <div id="welcome-back">
-        <span>bentornato {{$restaurant -> business_name}}!</span> 
+        <span>Bentornato {{$restaurant -> business_name}}!</span> 
       </div>
       <div id="create-row">
         <div id="left-create">
@@ -19,44 +19,86 @@
       </div>
         {{-- le immagini andranno rese dinamiche --}}
         <div id="restaurant-cards">
+          {{-- @foreach ($restaurants as $restaurant)
           <div class="crd">
             <ul>
               <li><img src="{{asset('/storage/assets/pizza-placeholder.jpg')}}" alt="copertina ristorante"></li>
               <li><span>{{$restaurant -> business_name}}</span></li>
-              <li><p>{{$restaurant -> description}}</p></li>
+              <li id="restaurant-address"><i class="fas fa-map-marker-alt"></i><p>{{$restaurant -> address}}</p></li>
+            </ul>
+            <div class="btn-edit-del">
               <a id="edit-button" href={{ route('editRestaurantViewLink', $restaurant -> id)}}><i class="fas fa-edit"></i></a>
               <a id="delete-button" href={{ route('deleteRestaurantLink', $restaurant -> id)}}><i class="fas fa-trash-alt"></i></a>
+            </div>
+          </div>
+          @endforeach --}}
+          <div class="crd">
+            <ul>
+              <li><img src="{{asset('/storage/assets/pizza-placeholder.jpg')}}" alt="copertina ristorante"></li>
+              <li><span>{{$restaurant -> business_name}}</span></li>
+              <li id="restaurant-address"><i class="fas fa-map-marker-alt"></i><p>{{$restaurant -> address}}</p></li>
             </ul>
+            <div class="btn-edit-del">
+              <a id="edit-button" href={{ route('editRestaurantViewLink', $restaurant -> id)}}><i class="fas fa-edit"></i></a>
+              <a id="delete-button" href={{ route('deleteRestaurantLink', $restaurant -> id)}}><i class="fas fa-trash-alt"></i></a>
+            </div>
           </div>
           <div class="crd">
             <ul>
               <li><img src="{{asset('/storage/assets/pizza-placeholder.jpg')}}" alt="copertina ristorante"></li>
               <li><span>{{$restaurant -> business_name}}</span></li>
-              <li><p>{{$restaurant -> description}}</p></li>
+              <li id="restaurant-address"><i class="fas fa-map-marker-alt"></i><p>{{$restaurant -> address}}</p></li>
+            </ul>
+            <div class="btn-edit-del">
               <a id="edit-button" href={{ route('editRestaurantViewLink', $restaurant -> id)}}><i class="fas fa-edit"></i></a>
               <a id="delete-button" href={{ route('deleteRestaurantLink', $restaurant -> id)}}><i class="fas fa-trash-alt"></i></a>
-            </ul>
+            </div>
           </div>
           <div class="crd">
             <ul>
               <li><img src="{{asset('/storage/assets/pizza-placeholder.jpg')}}" alt="copertina ristorante"></li>
               <li><span>{{$restaurant -> business_name}}</span></li>
-              <li><p>{{$restaurant -> description}}</p></li>
+              <li id="restaurant-address"><i class="fas fa-map-marker-alt"></i><p>{{$restaurant -> address}}</p></li>
+            </ul>
+            <div class="btn-edit-del">
               <a id="edit-button" href={{ route('editRestaurantViewLink', $restaurant -> id)}}><i class="fas fa-edit"></i></a>
               <a id="delete-button" href={{ route('deleteRestaurantLink', $restaurant -> id)}}><i class="fas fa-trash-alt"></i></a>
-            </ul>
+            </div>
           </div>
           <div class="crd">
             <ul>
               <li><img src="{{asset('/storage/assets/pizza-placeholder.jpg')}}" alt="copertina ristorante"></li>
               <li><span>{{$restaurant -> business_name}}</span></li>
-              <li><p>{{$restaurant -> description}}</p></li>
+              <li id="restaurant-address"><i class="fas fa-map-marker-alt"></i><p>{{$restaurant -> address}}</p></li>
+            </ul>
+            <div class="btn-edit-del">
               <a id="edit-button" href={{ route('editRestaurantViewLink', $restaurant -> id)}}><i class="fas fa-edit"></i></a>
               <a id="delete-button" href={{ route('deleteRestaurantLink', $restaurant -> id)}}><i class="fas fa-trash-alt"></i></a>
+            </div>
+          </div>
+          <div class="crd">
+            <ul>
+              <li><img src="{{asset('/storage/assets/pizza-placeholder.jpg')}}" alt="copertina ristorante"></li>
+              <li><span>{{$restaurant -> business_name}}</span></li>
+              <li id="restaurant-address"><i class="fas fa-map-marker-alt"></i><p>{{$restaurant -> address}}</p></li>
             </ul>
+            <div class="btn-edit-del">
+              <a id="edit-button" href={{ route('editRestaurantViewLink', $restaurant -> id)}}><i class="fas fa-edit"></i></a>
+              <a id="delete-button" href={{ route('deleteRestaurantLink', $restaurant -> id)}}><i class="fas fa-trash-alt"></i></a>
+            </div>
+          </div>
+          <div class="crd">
+            <ul>
+              <li><img src="{{asset('/storage/assets/pizza-placeholder.jpg')}}" alt="copertina ristorante"></li>
+              <li><span>{{$restaurant -> business_name}}</span></li>
+              <li id="restaurant-address"><i class="fas fa-map-marker-alt"></i><p>{{$restaurant -> address}}</p></li>
+            </ul>
+            <div class="btn-edit-del">
+              <a id="edit-button" href={{ route('editRestaurantViewLink', $restaurant -> id)}}><i class="fas fa-edit"></i></a>
+              <a id="delete-button" href={{ route('deleteRestaurantLink', $restaurant -> id)}}><i class="fas fa-trash-alt"></i></a>
+            </div>
           </div>
         </div>
     </section>
-    
   </main>
 @endsection
