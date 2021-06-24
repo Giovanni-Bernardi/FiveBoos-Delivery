bb698469307f4b72fed90156c4e5fd7c88b8ac95
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-06-24 09:10:30 +0200

    Fixed js, scss

--- public/js/app.js
+++ public/js/app.js
@@ -49675,7 +49675,47 @@ window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.
 
  //mouse follow
 
-var cursorFollow = document.querySelectorAll('.popup-note'); // document.addEventListener('mousemove', fn, false);
+var cursorFollow = document.querySelectorAll('.popup-note'); //TEST FUNZIONE SCOLL 1
+
+$(document).on('scroll', function (e) {
+  var value = $(this).scrollTop();
+
+  if (value < 100) {
+    $("header").css("background", "transparent");
+  } else {
+    $("header").css("background", "red");
+  }
+}); // TEST FUNZIONE SCOLL 2
+// window.onscroll = function() {scrollFunction()};
+//  function scrollFunction() {
+//    if (document.body.scrollTop >80 || document.documentElement.scrollTop >80) {
+//      $('nav').addClass('navbar-shrink');
+//    } else {
+//      $('nav').removeClass('navbar-shrink');
+//    }
+// }
+
+function PopupModal() {
+  var modal = document.getElementById("myModal");
+  var btn = document.getElementById("myBtn");
+  var span = document.getElementsByClassName("close")[0];
+
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+}
+
+document.addEventListener('DOMContentLoaded', PopupModal);
 
 /***/ }),
 
