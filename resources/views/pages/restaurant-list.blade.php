5de3cd9af23b8a9603f47bd0a1df8ce35c2b5c17
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-07-01 08:30:46 +0200

    Ricerca veloce ottimizzata

--- resources/views/pages/restaurant-list.blade.php
+++ resources/views/pages/restaurant-list.blade.php
@@ -7,7 +7,7 @@
 <main>
     {{-- VUE Riecerca dinamica --}}
     <div id="appSearch"> 
-        <!-- {{-- restaurant list --}} -->
+        {{-- Filtro categorie --}}
         <div class="list-container">
             <div id="app-categories" class="list-jumbotron">
                 <ul>
@@ -22,10 +22,7 @@
             </div>
         </div>
 
-        {{-- <div class="titles-restaurant-list">
-            
-        </div> --}}
-
+        {{-- Lista ristoranti dinamica - ricerca veloce --}}
         <ul class="restaurants-box">
             <li v-if="filter == false" class="titles-restaurant-list">
                 <h2>Ristoranti in zona</h2>
@@ -33,22 +30,6 @@
             <li v-else class="titles-restaurant-list">
                 <h2>Risultato ricerca</h2>
             </li>
-            {{-- <ul v-if="filter == false">
-                <li v-for="restaurant in restaurantsList">
-                    Name: @{{restaurant.business_name}}
-                    <strong v-for="genre in restaurant.categories">
-                        - @{{genre.name}}
-                    </strong>
-                </li>
-            </ul>
-            <ul v-else>
-                <li v-for="restaurant in filteredRestaurants">
-                    Name: @{{restaurant.business_name}} (Genres: 
-                    <strong v-for="genre in restaurant.categories">
-                        - @{{genre.name}}
-                    </strong>)
-                </li>
-            </ul> --}}
             <li class="restaurant-card" v-for="restaurant in filteredRestaurants">
                 <a :href="'/restaurant/' + restaurant.id">
                     <div class="background-image">
@@ -67,33 +48,7 @@
                     </div>
                 </a>   
             </li>
-                
         </ul>
-        {{-- <ul class="restaurants-box" v-else>
-            <li class="titles-restaurant-list">
-                <h2>Risultato riecerca</h2>
-            </li>
-
-            <li class="restaurant-card" v-for="restaurant in filteredRestaurants">
-                <a :href="'/restaurant/' + restaurant.id">
-                    <div class="background-image">
-                        <img src="{{asset('/storage/assets/pizza-placeholder.jpg')}}" alt="">
-                        
-                        <div class="categories-card">
-                            {{-- categories placeholder --}}
-                            {{-- <h3>
-                                @{{restaurant.business_name}}
-                            </h3>
-                            <ul class="categories-container">
-                                <li class="category-badge" v-for="category in restaurant.categories">
-                                    @{{category.name}} 
-                                </li>
-                            </ul>
-                        </div> 
-                    </div>
-                </a>   
-            </li>
-        </ul> --}}
     </div>
 </main>
     
