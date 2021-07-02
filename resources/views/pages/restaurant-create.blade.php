0d23b7a053a31cb5d1f5546d32b444e045b56495
Vladyslav Okopnyy <harmfulcati@gmail.com>
2021-07-02 18:26:34 +0300

    Page create restaurant responsive

--- resources/views/pages/restaurant-create.blade.php
+++ resources/views/pages/restaurant-create.blade.php
@@ -14,12 +14,16 @@
 <main class="create-restaurant">
     <div class="container">
         <div class="separation-content">
+            <div class="logo-image">
+                <img id="logo-img" src="{{asset('/storage/assets/site-logo/create.svg')}}" alt="logo">
+            </div>
+
             <form method="POST" action="{{ route('storeRestaurant') }}" enctype="multipart/form-data" id="new-restaurant">
                 @csrf
                 @method('POST')
                 <ul>
                     <li>
-                        <h2>Crea il tuo ristorante)</h2>
+                        <h2>Crea il tuo ristorante</h2>
                     </li>
                     <li>
                         <label>Nome del ristorante</label>
@@ -42,7 +46,7 @@
                     <li>
                         <label>Descrizione</label>
                         <div class="">
-                            <textarea spellcheck="false" rows="8" name="description" class="text" required></textarea>
+                            <textarea spellcheck="false" rows="4" name="description" class="text" required></textarea>
                         </div>
                     </li>
                     <li>
@@ -64,24 +68,19 @@
                                 <div class="category">
                                     <input id="cat-{{$category -> id}}" type="checkbox" name="category_id[]" value="{{$category -> id}}">
                                     <label for="cat-{{$category -> id}}" class="circle"></label>
-                                    <label for="categories[]">
+                                    <label class="name-category" for="categories[]">
                                         {{$category -> name}}
                                     </label>
                                 </div>
                             @endforeach
                         </div>
                     </li>
-
                 </ul>
+                <div class="submit">
+                    <a class="button-back" href='{{ route('restaurantProfileViewLink')}}'>Lista ristoranti</a>
+                    <button class="button-create" type="submit" class="btn btn-primary" form="new-restaurant">Crea Ristorante</button>
+                </div>
             </form>
-            <div class="">
-                <h1>Imagine</h1>
-                <img src="" alt="">
-            </div>
-        </div>
-        <div class="submit">
-            <a class="button-back" href='{{ route('restaurantProfileViewLink')}}'>Lista ristoranti</a>
-            <button class="button-create" type="submit" class="btn btn-primary" form="new-restaurant">Crea Ristorante</button>
         </div>
     </div>
 </main>
